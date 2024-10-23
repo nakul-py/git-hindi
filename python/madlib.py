593ef6693f2426b6518526d7364f5e4a827c6442
nakul-py <nakulverma.py@gmail.com>
2024-10-23 17:06:36 +0530

    add a word game

---
+++ python/madlib.py
@@ -0,0 +1,14 @@
+# Madlibs game
+# word game where you create a story by filling the random words
+
+adjective1 = input("Enter an adjective (description): ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective (description): ")
+verb1 = input("Enter a verb ending with 'ing'")
+adjective3 = input("Enter an adjective (description): ")
+
+
+print(f"Today, I played a cricket {adjective1} match ")
+print(f"I took 29 runs in a {noun1}")
+print(f"that was a {adjective2} {noun1} for me and my{verb1}")
+print(f"I was {adjective3}!")
