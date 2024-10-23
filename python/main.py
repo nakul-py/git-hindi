161384cb946a8f290786c17baaef4f3ea80ed083
nakul-py <nakulverma.py@gmail.com>
2024-10-23 13:08:01 +0530

    adding typescricpt op in pyhton

--- python/main.py
+++ python/main.py
@@ -1,51 +1,67 @@
 
-# print ("I like coding!")
-# print("this is helpful")
+# print ("HI THERE!")
+# print("I AM NAKUL")
 
 # Variable = A container for a value (string, integer, float, boolean)
 #            Avariable behaves as if it was the value it contains
  
 # Strings
 
-first_name = "Nakul"
-fruit = "mango"
-email = "nakul@gmail.com"
+#first_name = "Nakul"
+#fruit = "mango"
+#email = "nakul@gmail.com"
 
-print(f"Hello {first_name}")
-print(f"You like {fruit}")
-print(f"Your email is: {email}") 
+#print(f"Hello {first_name}")
+#print(f"You like {fruit}")
+#print(f"Your email is: {email}") 
 
 # Integers
 
-age = 20
-height = "200m"
-date_of_birth = "1/2/2000"
+#age = 20
+#height = "200m"
+#date_of_birth = "1/2/2000"
 
-print(f"Your age is: {age}")
-print(f"Your height is: {height}")
-print(f"Your date of birth is: {date_of_birth}")
+#print(f"Your age is: {age}")
+#print(f"Your height is: {height}")
+#print(f"Your date of birth is: {date_of_birth}")
 
 # Floats
 
-price = 20.6
-gst = 2.00
-distance = 2.5
+#price = 20.6
+#gst = 2.00
+#distance = 2.5
 
-print(f"The price is {price}")
-print(f"GST on product is {gst}")
-print(f"Distance from your home to supermarket {distance}km")
+#print(f"The price is {price}")
+#print(f"GST on product is {gst}")
+#print(f"Distance from your home to supermarket {distance}km")
 
 # Booleans
 
+#is_student = True
+#for_sale = False
+
+#if is_student:
+#    print("You are a student")
+#else:
+#    print("You are not a student")
+
+#if for_sale:
+#    print("This item is for sale")
+#else:
+#   print("This item is Not available")    
+
+# Typescript = the process of converting a variable from one datatype to another
+#              str(), int(), float(), bool()
+
+name = "Nakul Verma"
+age = 25
+height = 6.6
 is_student = True
-for_sale = False
 
-if is_student:
-    print("You are a student")
-else:
-    print("You are not a student")
+age = float(age)
 
-if for_sale:
-    print("This item is for sale")
-else:
-    print("This item is Not available")    
+print(age)
+
+name = bool(name)
+
+print(name)
