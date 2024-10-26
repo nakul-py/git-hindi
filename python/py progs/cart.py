d52d243eab6d480854b4094c3a4d115f88e73c05
nakul-py <nakulverma.py@gmail.com>
2024-10-26 18:53:00 +0530

    Adding Shopping Cart Program

---
+++ python/py progs/cart.py
@@ -0,0 +1,26 @@
+## Shopping cart program
+
+foods = []
+prices = []
+total = 0
+
+while True:
+    food = input("Enter a food to buy (q to quit): ")
+    if food.lower() == "q":
+        break
+    else:
+        price = float(input(f"Enter the price of a {food}: $"))
+        foods.append(food)
+        prices.append(price)
+
+print("------YOUR CART ------")
+
+for food in foods:
+    print(food, end = ", ")
+
+for price in prices:
+    total += price
+
+print()
+
+print(f"Your total price is: {total}$")
