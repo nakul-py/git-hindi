b3f5c385ff2a900d82d28aae6093c83faa151db7
nakul-py <nakulverma.py@gmail.com>
2024-11-06 17:48:49 +0530

    adding arithmatic operators

--- javascript/basic_of_js/index.js
+++ javascript/basic_of_js/index.js
@@ -29,6 +29,62 @@ document.getElementById("p1").textContent = "Hi.";
 //console.log(`your name is: ${firstname}`);
 //console.log(`the price is: ${price}`);
 
-let online = false;
+//let online = false;
 
-console.log(`Bro is online: ${online}`);
+//console.log(`Bro is online: ${online}`);
+
+// arithmatic operators = operands (values, variables, etc)
+//                        operators (+ - * /)
+//                        ex. 11 = x + 5
+
+let numbers = 30;
+
+//numbers = numbers + 2;
+//numbers = numbers - 5;
+//numbers = numbers * 2;
+//numbers = numbers / 2;
+//numbers = numbers ** 3;
+//numbers = numbers % 4;
+
+//numbers += 2;
+//numbers -= 5;
+//numbers *= 3;
+//numbers /= 2;
+//numbers **= 2;
+//numbers %= 3;
+
+//numbers++;
+//numbers--;
+
+//console.log(numbers);
+
+/*
+    operator precedence
+    1. parenthesis ()
+    2. exponents
+    3. multiplication and division and modulo
+    4. addiditon and subctraction
+*/
+
+//let result = 2 + 15 + 49;
+
+//console.log(result);
+
+// How to accept user input
+
+// 1. Easy way = window prompt
+// 2. Professional way = HTML textbox
+
+///// EASY WAY
+//let username; 
+
+//username = window.prompt("what is your name?");
+
+//console.log(username);
+
+//// PROFESSIONAAL WAY
+
+document.getElementById("mySubmit").onclick = function(){
+    username = document.getElementById("myText").value;
+    document.getElementById("myname").textContent = `Hello ${username}`
+}
