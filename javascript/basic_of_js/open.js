b3683a6ee2044d76ecd0f0c178ae7f8a1630254d
nakul-py <nakulverma.py@gmail.com>
2024-11-08 12:24:52 +0530

    Adding Random number generator and some math functions

--- javascript/basic_of_js/open.js
+++ javascript/basic_of_js/open.js
@@ -22,7 +22,7 @@ console.log(z, typeof z);*/
 
 // const = a vvariable that can't be changed
 
-const PI = 3.14285;
+/*const PI = 3.14285;
 let radius;
 let circumference;
 
@@ -33,4 +33,47 @@ document.getElementById("mySubmit").onclick = function(){
     radius = Number(radius);
     circumference = 2 * PI * radius;
     document.getElementById("myh3").textContent = circumference +"cm";
+} */
+
+// Math = built-in object function that provides 
+//        a collection of properties and methods.
+
+//let x = 1;
+//let y = 2;
+//let z = 4;
+
+//z = Math.round(x);
+//z = Math.floor(x);
+//z = Math.ceil(x);
+//z = Math.trunc(x);
+//z = Math.pow(y, x);
+//z = Math.sqrt(x);
+//z = Math.log(x);
+//z = Math.sin(x);
+//z = Math.cos(x);
+//z = Math.tan(x);
+
+//let max = Math.max(x, y, z);
+//let min = Math.min(x, y, z);
+//console.log(max);
+
+// RANDOM NUMBER GENERATOR
+
+/*
+const min = 50;
+const max = 100;
+
+let randomNum = Math.floor(Math.random() * ((max + 1) - min)) + min ;
+ 
+console.log(randomNum)*/
+
+const myButton = document.getElementById("myButton");
+const myLabel = document.getElementById("myLabel");
+const min = 1;
+const max = 6;
+let randomNum;
+
+myButton.onclick = function(){
+    randomNum = Math.floor(Math.random() * max) + min;
+    myLabel.textContent = randomNum;
 }
