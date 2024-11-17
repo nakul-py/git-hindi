2622ea5fa9af848a0ae45b4e3a53bc163bcb9941
nakul-py <nakulverma.py@gmail.com>
2024-11-17 17:15:28 +0530

    adding map function

--- javascript/basic_of_js/open.js
+++ javascript/basic_of_js/open.js
@@ -392,3 +392,21 @@ function capitalize(element , index, array){
 function display(element){
     console.log(element);
 }*/
+
+//----------------------------------------------------------
+
+// .map() = accepts a callback and applies that function
+//          to each element of an array, then return a new array
+
+const numbers = [1, 2, 3, 4, 5];
+const squares = numbers.map(square);
+const cubes = numbers.map(cube);
+console.log(cubes)
+
+function square(element){
+    return Math.pow(element, 2)
+}
+
+function cube(element){
+    return Math.pow(element, 3)
+}
